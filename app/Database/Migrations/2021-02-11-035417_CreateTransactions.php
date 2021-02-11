9b317301c08af780f8ea988da610cb02fd0ff360
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-02-11 11:22:08 +0700

    add tables

---
+++ app/Database/Migrations/2021-02-11-035417_CreateTransactions.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTransactions extends Migration
+{
+  public function up()
+  {
+    $this->forge->addField([
+      'id'          => [
+        'type'           => 'INT',
+        'unsigned'       => true,
+        'auto_increment' => true,
+      ],
+      'supplier_id'          => [
+        'type'           => 'INT',
+        'unsigned'       => true,
+      ],
+      'medicine_id'          => [
+        'type'           => 'INT',
+        'unsigned'       => true,
+      ],
+      'type'        => [
+        'type'           => 'ENUM("buy","sell")',
+        'default'        => 'buy',
+        'null'           => false,
+      ],
+      'quantity'          => [
+        'type'           => 'INT',
+        'unsigned'       => true,
+      ],
+      'price'          => [
+        'type'           => 'INT',
+        'unsigned'       => true,
+      ],
+      'created_at datetime default current_timestamp',
+      'updated_at datetime default current_timestamp on update current_timestamp',
+    ]);
+    $this->forge->addKey('id', true);
+    $this->forge->addForeignKey('supplier_id', 'suppliers', 'id');
+    $this->forge->addForeignKey('medicine_id', 'medicines', 'id');
+    $this->forge->createTable('transactions');
+  }
+
+  public function down()
+  {
+    $this->forge->dropTable('transactions');
+  }
+}
